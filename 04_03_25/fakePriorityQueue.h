419be10807231a5108ad272eda94ebaa8e4b9819
Acatlin32 <acatlin32@gmail.com>
2025-04-05 12:14:00 -0400

    Update fakePriorityQueue.h
    Updated fakePriorityQueue enqueue to have priority

--- 04_03_25/fakePriorityQueue.h
+++ 04_03_25/fakePriorityQueue.h
@@ -13,19 +13,36 @@ public:
 template <class t>
 void fakePriorityQueue<t>::enqueue(const t &queueElement)
 {
-    node<t> *newNode;
-    newNode = new node<t>;
+    node<t> *newNode = new node<t>;            //make new node
     newNode->data = new t(queueElement);
     newNode->link = nullptr;
-    if (this->isEmptyQueue())
-    {
-        this->queueFront = newNode;
+
+    if (this->isEmptyQueue()){
+        this->queueFront = newNode;            //if empty both pointers point at the first new node
         this->queueRear = newNode;
     }
-    else
-    {
-        this->queueRear->link = newNode;
-        this->queueRear = newNode;
+    else{
+        node<t> *current = this->queueFront;        //current points to the front
+        node<t> *previous = nullptr;                //previous points to the end
+
+        while (current != nullptr && queueElement <= *(current->data)){
+            previous = current;                    //this loop traverses the list comparing data
+            current = current->link;
+        }
+
+        if (previous == nullptr){                   //insert at the front if loop completed(back to nullptr)
+            newNode->link = this->queueFront;
+            this->queueFront = newNode;
+        }
+        else{                                         //insert whereever *current landed
+            newNode->link = current;
+            previous->link = newNode;
+        }
+
+        if (newNode->link == nullptr)                //update queueRear if the new node was at the end
+        {
+            this->queueRear = newNode;
+        }
     }
 }
 
